8367ed0c7cbf4ea27fc114c851a97904449ab0bc
Kyle-Desktop <kmelik@live.com>
2021-09-21 12:45:40 -0700

    Updated dependencies.

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Test = () => {
 
     return (
-        <div className="txt_center">Hello World</div>
+        <div className="txt_center">Hello World, im a test</div>
     )
 }
 
